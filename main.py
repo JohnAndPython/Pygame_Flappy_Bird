b93cf0b6dd85cb727e3fe96c2974d3b45aa2489c
JohnJames <barthos@gmx.de>
2024-11-22 18:20:27 +0100

    Minor Changes

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 # Create initial pipes
-pipe_1 = Bar(screen, (0,0,0), 80, 150, (450, 0))
+pipe_1 = Bar(screen, (200,0,0), 80, 150, (450, 0))
 pipe_1.rect.centerx = 500
 pipe_1.rect.top = 0
 
-pipe_2 = Bar(screen, (0,0,0), 80, 300, (450, 0))
+pipe_2 = Bar(screen, (50,0,0), 80, 300, (450, 0))
 pipe_2.rect.centerx = 500
-pipe_2.rect.bottom = 600
+pipe_2.rect.bottom = HEIGHT
 
-pipe_3 = Bar(screen, (0,0,0), 80, 200, (700, 0))
+pipe_3 = Bar(screen, (0,0,200), 80, 200, (700, 0))
 pipe_3.rect.centerx = 780
 pipe_3.rect.top = 0
 
-pipe_4 = Bar(screen, (0,0,0), 80, 150, (700, 0))
+pipe_4 = Bar(screen, (0,0,50), 80, 150, (700, 0))
 pipe_4.rect.centerx = 780
-pipe_4.rect.bottom = 600
+pipe_4.rect.bottom = HEIGHT
 
 pipe_grp = pygame.sprite.Group()
 pipe_grp.add((pipe_1, pipe_2, pipe_3, pipe_4))
@@ -41,6 +41,7 @@ pl_grp = pygame.sprite.GroupSingle(player_1)
 
 dictus = dict()
 
+gap = 150
 
 while True:
 
@@ -58,24 +59,32 @@ while True:
     
 
 
-    for bar in pipe_grp:
+    for index, bar in enumerate(pipe_grp):
         if bar.rect.centerx <= -80:
             bar.kill()
-            new_height = random.randint(50, 350)
-            bar.chg_size(new_height)
-            bar.rect.top = 0
+            print(pipe_grp)
+            new_height = 0
+            if index == 0:
+                new_height = random.randint(50, 350)
+                bar.chg_size(new_height)
+                bar.rect.top = 0
+            elif index == 1:
+                bar.chg_size(HEIGHT - new_height - gap)
+                bar.rect.bottom = HEIGHT
+
+
+
             print(bar.rect.height)
             bar.rect.centerx = 440
             pipe_grp.add(bar)
+            print(pipe_grp)
 
 
 
     pipe_grp.update()
     pipe_grp.draw(screen)
 
-        
     player_1.update()
-    #player_1.move_down()
     player_1.draw()
     
     dictus = pygame.sprite.groupcollide(pipe_grp, pl_grp, False, False)
